794c87ad8d47855de2962be57db39a522d7eba8f
Lokathor <zefria@gmail.com>
2020-02-17 19:25:17 -0700

    Update to allow building on wasm32-unknown-unknown

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,25 @@
 //! * If you're on not-Windows it re-exports from `libc`.
 //!
 //! You might think "why not just always link to `libc`?", but on Windows that
-//! means that your resulting binary is less portable for no reason. Either the
-//! user's machine will need to have the visual studio redistributable DLL
-//! installed or you'll have to build the binary with a static linked CRT
-//! configured. Either of these options are silly if all that you want is some
-//! type declarations and you're not even calling any functions.
-//! 
+//! means that your resulting binary is just a little less portable for no
+//! reason. Either the user's machine will need to have the visual studio
+//! redistributable DLL installed or you'll have to build the binary with a
+//! static linked CRT configured. Either of these options are silly if all that
+//! you want is some type declarations and you're not even calling any
+//! functions.
+//!
 //! ### FAQ
-//! 
+//!
 //! * **Question:** Lokathor, aren't you just being totally crazy?
 //!   * Yes.
+//! * **Question:** What do you do about `wasm32-unknown-unknown`? Unlike
+//!   `-wasi` and `-emscripten`, it has no `libc` support, so having "C types"
+//!   doesn't make any sense, right?
+//!   * Correct, there is no `libc`. In this situation, essentially no
+//!     definition is actually _wrong_, so all that matters is that _anything_
+//!     is provided. For simplicity sake, you get the same definitions as you
+//!     get on a `windows` target. If a `libc` is created in the future for that
+//!     target, the types will be adjusted to match if necessary.
 
 /// Does all our conditional compilation selection.
 #[macro_export]
@@ -73,7 +82,13 @@ macro_rules! pick {
 pub use core::ffi::c_void;
 
 pick! {
-  if #[cfg(windows)] {
+  if #[cfg(any(
+    windows,
+    all(
+      target_arch = "wasm32",
+      not(any(target_env = "wasi", target_env = "emscripten"))
+    )
+  ))] {
     pub type c_char = i8;
     pub type c_schar = i8;
     pub type c_uchar = u8;
--- tests/types_present.rs
+++ tests/types_present.rs
@@ -1,4 +1,3 @@
-
 use chlorine::*;
 
 #[test]
