7ecfccaf3e6c0726833393a637a4a76640456cee
Lokathor <zefria@gmail.com>
2021-10-17 14:44:10 -0600

    update for latest info

--- src/lib.rs
+++ src/lib.rs
@@ -57,117 +57,26 @@ macro_rules! pick {
   };
 }
 
+// most of these never change
+
 pub use core::ffi::c_void;
-
-pick! {
-  // c_char
-  if #[cfg(any(
-    all(
-      target_os = "linux",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "hexagon",
-        target_arch = "powerpc",
-        target_arch = "powerpc64",
-        target_arch = "s390x",
-        target_arch = "riscv64",
-        target_arch = "riscv32"
-      )
-    ),
-    all(
-      target_os = "android",
-      any(target_arch = "aarch64", target_arch = "arm")
-    ),
-    all(target_os = "l4re", target_arch = "x86_64"),
-    all(
-      target_os = "freebsd",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "powerpc",
-        target_arch = "powerpc64"
-      )
-    ),
-    all(
-      target_os = "netbsd",
-      any(target_arch = "aarch64", target_arch = "arm", target_arch = "powerpc")
-    ),
-    all(target_os = "openbsd", target_arch = "aarch64"),
-    all(
-      target_os = "vxworks",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "powerpc64",
-        target_arch = "powerpc"
-      )
-    ),
-    all(target_os = "fuchsia", target_arch = "aarch64")
-  ))] {
-    pub type c_char = c_uchar;
-  } else if #[cfg(not(any(
-    all(
-      target_os = "linux",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "hexagon",
-        target_arch = "powerpc",
-        target_arch = "powerpc64",
-        target_arch = "s390x",
-        target_arch = "riscv64",
-        target_arch = "riscv32"
-      )
-    ),
-    all(
-      target_os = "android",
-      any(target_arch = "aarch64", target_arch = "arm")
-    ),
-    all(target_os = "l4re", target_arch = "x86_64"),
-    all(
-      target_os = "freebsd",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "powerpc",
-        target_arch = "powerpc64"
-      )
-    ),
-    all(
-      target_os = "netbsd",
-      any(target_arch = "aarch64", target_arch = "arm", target_arch = "powerpc")
-    ),
-    all(target_os = "openbsd", target_arch = "aarch64"),
-    all(
-      target_os = "vxworks",
-      any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "powerpc64",
-        target_arch = "powerpc"
-      )
-    ),
-    all(target_os = "fuchsia", target_arch = "aarch64")
-  )))] {
-    pub type c_char = c_schar;
-  } else {
-    compile_error!("The format of c_char is unknown!");
-  }
-}
-
 pub type c_schar = i8;
-
 pub type c_uchar = u8;
-
+// char depends on stuff
 pub type c_short = i16;
-
 pub type c_ushort = u16;
+// int/uint depends on stuff
+// long/ulong depends on stuff
+pub type c_longlong = i64;
+pub type c_ulonglong = u64;
+pub type c_float = f32;
+pub type c_double = f64;
 
+// c_char, c_int, and c_uint are set by the arch
+
+// first we define c_char
 pick! {
-  // c_int / c_uint
   if #[cfg(any(
-    target_arch = "aarch64",
     target_arch = "arm",
     target_arch = "asmjs",
     target_arch = "wasm32",
@@ -177,6 +86,47 @@ pick! {
     target_arch = "s390x",
     target_arch = "riscv32",
     target_arch = "riscv64",
+    target_arch = "aarch64",
+    target_arch = "msp430",
+  ))] {
+    // c_char is unsigned (unless macos)
+    pick! {
+      if #[cfg(target_os = "macos")] {
+        pub type c_char = c_schar;
+      } else {
+        pub type c_char = c_uchar;
+      }
+    }
+  } else if #[cfg(any(
+    target_arch = "mips",
+    target_arch = "mips64",
+    target_arch = "sparc64",
+    target_arch = "x86",
+    target_arch = "x86_64",
+    target_arch = "nvptx",
+    target_arch = "nvptx64",
+    target_arch = "xtensa",
+  ))] {
+    // c_char is signed
+    pub type c_char = c_schar;
+  } else {
+    compile_error!("The alias for c_char, c_int, and c_uint is unknown!");
+  }
+}
+
+// then we define c_int and c_uint
+pick! {
+  if #[cfg(any(
+    target_arch = "arm",
+    target_arch = "asmjs",
+    target_arch = "wasm32",
+    target_arch = "wasm64",
+    target_arch = "powerpc",
+    target_arch = "powerpc64",
+    target_arch = "s390x",
+    target_arch = "riscv32",
+    target_arch = "riscv64",
+    target_arch = "aarch64",
     target_arch = "mips",
     target_arch = "mips64",
     target_arch = "sparc64",
@@ -188,41 +138,45 @@ pick! {
   ))] {
     pub type c_int = i32;
     pub type c_uint = u32;
-  } else if #[cfg(target_arch = "msp430")] {
+  } else if #[cfg(any(
+    target_arch = "msp430",
+  ))] {
     pub type c_int = i16;
     pub type c_uint = u16;
   } else {
-    compile_error!("The format of c_int / c_uint is unknown!");
+    compile_error!("The alias for c_int and c_uint is unknown!");
   }
 }
 
+// c_long and c_ulong are set by the OS
 pick! {
-  // c_long / c_ulong
+  // in some special cases we ignore pointer size ...
   if #[cfg(windows)] {
     pub type c_long = i32;
     pub type c_ulong = u32;
-  } else if #[cfg(any(target_os = "redox", target_os = "solaris"))] {
+  } else if #[cfg(any(
+    target_os = "redox",
+    target_os = "solaris",
+  ))] {
     pub type c_long = i64;
     pub type c_ulong = u64;
-  } else if #[cfg(any(target_pointer_width = "16", target_pointer_width = "32"))] {
+  }
+  // but by default we match the size of a long to the size of a pointer
+  else if #[cfg(target_pointer_width = "64")] {
+    pub type c_long = i64;
+    pub type c_ulong = u64;
+  } else if #[cfg(any(
+    target_pointer_width = "16",
+    target_pointer_width = "32",
+  ))] {
     pub type c_long = i32;
     pub type c_ulong = u32;
-  } else if #[cfg(target_pointer_width = "64")] {
-    pub type c_long = i64;
-    pub type c_ulong = u64;
   } else {
-    compile_error!("The format of c_long / c_ulong is unknown!");
+    compile_error!("The alias for c_long and c_ulong is unknown!");
   }
 }
 
-pub type c_longlong = i64;
-
-pub type c_ulonglong = u64;
-
-pub type c_float = f32;
-
-pub type c_double = f64;
-
+// this requires a crate feature is all
 pick! {
   if #[cfg(feature = "int_extras")] {
     pub type intmax_t = i64;
